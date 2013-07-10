7ded84810dc193c2e580b99c941a4e36b062ca62
John Ceci <tigeryan55@gmail.com>
2013-07-10 19:26:47 +0000

    mod web.js

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+	fs.readFileSync('index.html',function (err,data)
+	{
+		if (err) throw err;
+		response.send(data.toString());
+	});
 });
 
 var port = process.env.PORT || 5000;
