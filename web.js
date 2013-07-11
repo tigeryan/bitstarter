d1a4d6c66c62677f13eb64afd35896ffd895e11a
John Ceci <tigeryan55@gmail.com>
2013-07-11 03:06:19 +0000

    web

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	
 	var text = fs.readFileSync('index.html','utf8');
-	response.send("umm");
+	response.send(text);
 });
 
 var port = process.env.PORT || 5000;
