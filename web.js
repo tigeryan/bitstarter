b8ef9d3a48b88d018b8eb0054e3b4a6121be40fe
John Ceci <tigeryan55@gmail.com>
2013-07-11 02:53:23 +0000

    web2

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	fs.readFileSync('index.html',function (err,data)
+	fs.readFileSync('index.html','utf8',function (err,data)
 	{
 		if (err) throw err;
 		//response.send(data.toString('utf-8'));
