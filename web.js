8ae82cabcd3ae5b903637d33b2fb6d2fab022787
John Ceci <tigeryan55@gmail.com>
2013-07-11 02:47:15 +0000

    edit

--- web.js
+++ web.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
 	fs.readFileSync('index.html',function (err,data)
 	{
 		if (err) throw err;
-		response.send(data.toString('utf-8'));
+		//response.send(data.toString('utf-8'));
+		response.send("test");
 	});
 });
 
