72f525f8206c66672b58a5c0e50e31bae68631d5
John Ceci <tigeryan55@gmail.com>
2013-07-23 21:08:09 +0000

    base site

--- web.js
+++ web.js
@@ -8,8 +8,17 @@ app.get('/', function(request, response) {
 	var text = fs.readFileSync('index.html','utf8');
 	response.send(text);
 });
+app.get('/about.html',function(request, response) {
+	var text = fs.readFileSync('about.html','utf8');
+	response.send(text);
+});
+app.get('/contact.html',function(request,response) {
+	var text = fs.readFileSync('contact.html','utf8);
+	reponse.send(text);
+});
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
