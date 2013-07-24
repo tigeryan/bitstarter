223e618a5b61995b05fd041bee3c2711e73ab160
John Ceci <jceci@usa.m3.com>
2013-07-24 17:11:50 -0400

    require mailer

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var mailer = require('mailer');
 
 var app = express.createServer(express.logger());
 app.use("/css", express.static(__dirname + '/css'));
